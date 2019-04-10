f78fe18c81aeb8a73990abefacf215d1dfdc84cb
nc585 <nc585@georgetown.edu>
2019-04-10 11:07:12 -0400

    event selection

--- myscript.py
+++ myscript.py
@@ -40,12 +40,6 @@ def main():
     print("* * * * * * * * * * * * * * * * *")
     print(" ")
 
-    #make this dynamic 
-    print("Have you added the official Georgetown University Google Calendar to your personal Gmail account?")
-    
-    print("You can access the calendar here: https://guevents.georgetown.edu/calendar")
-    print(" ")
-
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
     print('Here are the next 10 events in the Georgetown Calendar:')
@@ -58,11 +52,25 @@ def main():
     events = events_result.get('items', [])
     for event in events:
         start = event['start'].get('dateTime', event['start'].get('date'))
+        # TODO: add an event number if possible
         print(start, event['summary'])
     
-    print(" ")
-    print("Would you like to know more about these events? If so, please enter the number you are interested in. For example, the first event listed would be '1' ")
+    print(" ")    
+    event_descriptions = []
+    while True:
+        selected_event = input("Would you like to know more about these events? If so, please select an event (1-10) or 'DONE' if there are no more events: ")
+        if selected_event == "DONE":
+            break
+        if not selected_event.isdigit():
+            print("Please enter a valid event number")
+        if int(selected_event) not in range (1,11):
+            print("Please enter a valid product id from 1 to 10")
+        else:  
+            event_descriptions.append(selected_event)
+    
     #print only one of the Georgetown Calendar events 
+
+
     #todo: change [0] to inputted number from user - print(events_result['items'][0]['description'])
 
 if __name__ == '__main__':
