bf0dd00a2d8d554d13bcfb7f4ed6aa7fb99b54a0
nc585 <nc585@georgetown.edu>
2019-04-14 16:42:49 -0400

    create event

--- myscript.py
+++ myscript.py
@@ -55,19 +55,17 @@ def main():
         start = event['start'].get('dateTime', event['start'].get('date'))
         # TODO: add an event number if possible
         print(start, event['summary'])
-
-   # print(events_result['items'][0])    
-    
+        
     print(" ")    
     event_descriptions = []
     while True:
-        selected_event = input("Would you like to know more about these events? If so, please select an event (1-10) or 'X' if there are no more events: ")
-        if selected_event == "X":
+        selected_event = input("Would you like to know more about these events? If so, please select an event (1-10) or 'DONE' if there are no more events: ")
+        if selected_event == "DONE":
             break
         if not selected_event.isdigit():
-            print("Please enter a valid event number")
+            print("Please enter a valid event number.")
         if int(selected_event) not in range (1,11):
-            print("Please enter a valid product id from 1 to 10")
+            print("Please enter a valid event number from 1 to 10.")
         else:  
             event_descriptions.append(selected_event)
     
@@ -78,7 +76,35 @@ def main():
         print(events_result['items'][int(selected_event)-1]['description'])
         print("------------")
 
-    #todo: change [0] to inputted number from user - print(events_result['items'][0]['description'])
+#TODO: give user option to create event reminder
+    while True:
+        create_event = input("Would you like to create an event reminder in your Google calendar? If so, please type 'YES'. If not, please type 'NO'.")
+        if create_event == "NO":
+            break
+        else:  
+            event_summary = input("Please enter event name:")
+            event_location = input("Please enter event location:")
+            event_desc = input("Please enter event description of your choice:")
+            event_start = input("Please enter event start date in the following format, YYYY-MM-DD:")
+            event_time_start = input ("Please enter start time of event in the following format, HH-MM-SS:")
+            event_end = input("Please enter event end date in the following format, YYYY-MM-DD:")
+            event_time_end = input ("Please enter end time of event in the following format, HH-MM-SS:")
+            event = {
+                'summary': event_summary,
+                'location': event_location,
+                'description': event_desc,
+                'start': {
+                    'dateTime': event_start + 'T' + event_time_start, 
+                    'timeZone': 'America/New_York',
+                },
+                'end': {
+                    'dateTime': event_end + 'T' + event_time_end, 
+                    'timeZone': 'America/New_York',
+                },
+            }
+
+            event = service.events().insert(calendarId='primary', body=event).execute()
+            print('Event created: %s' % (event.get('htmlLink')))
 
 if __name__ == '__main__':
     main()
